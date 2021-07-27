df8a615fe2e00769c9b7bdbd85ae219903518fbe
Huy HO <hghuyho@gmail.com>
2021-07-27 20:46:51 +0700

    First commit

---
+++ cards/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{"Ace of Spades", newCard()}
+	cards = append(cards, "Six of Spades")
+	fmt.Println(cards)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
