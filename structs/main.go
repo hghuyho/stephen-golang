eab97db8a3d2cd1fe83b8bd084e5a865d640e3e9
Huy HO <hghuyho@gmail.com>
2021-07-29 16:10:16 +0700

    Embedding Structs

--- structs/main.go
+++ structs/main.go
@@ -2,18 +2,33 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// huy := person{lastName: "Huy", firstName: "HO"}
 	// huy = person{lastName: "Huy", firstName: "HOO"}
 
-	var huy person
-	huy.firstName = "Huy"
-	huy.lastName = "HO"
+	// var huy person
+	// huy.firstName = "Huy"
+	// huy.lastName = "HO"
+
+	huy := person{
+		firstName: "Huy",
+		lastName:  "HO",
+		contact: contactInfo{
+			email:   "hghuyho@secman.vn",
+			zipCode: 700000,
+		},
+	}
 
 	fmt.Println(huy)
 	fmt.Printf("%+v", huy)
